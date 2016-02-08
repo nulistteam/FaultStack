e49b6aff0e5a1bb24e6683724773bf47e82e9374
SHvsMK <SHvsMK@gmail.com>
2016-02-07 21:32:37 -0600

    Delete Requirement.py

--- Requirement.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'yichengwang'
-
-import logging
-
-class requirements():
-
-    def get(self, role):
-        #according to different test and role to set the test for the requirement
-        pass
